8d74f74340eff8017a4c821542cc01f0e7fbdaaa
Nick Houghton <nhoughto5@gmail.com>
2020-11-19 21:36:58 -0800

    DFS

--- Empire/Empire/NHoughtonIM.c
+++ Empire/Empire/NHoughtonIM.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #define LINE_SIZE 20
 
 // ====================================== //
 // Utilities
 // ====================================== //
-inline int readNumber(char* input)
-{
+inline int readNumber(char* input) {
     int ret;
     //sscanf should fill a single variable (i.e. N), if not something has gone wrong 
     if (sscanf(input, "%d", &ret) != 1) return -1;
     return ret;
 }
 
-int* readLine(char* input, int* row, int size)
-{
+int* readLine(char* input, int* row, int size) {
     char* str = strtok(input, " ");
     int i = 0;
-    while (str != NULL)
-    {
+    while (str != NULL) {
         int value = readNumber(str);
         row[i] = value;
         str = strtok(NULL, " ");
@@ -28,13 +26,10 @@ int* readLine(char* input, int* row, int size)
     return row;
 }
 
-void testMatrix(int** matrix, int size)
-{
+void testMatrix(int** matrix, int size) {
     printf("Test\n");
-    for (int i = 0; i < size; ++i)
-    {
-        for (int j = 0; j <= i; ++j)
-        {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j <= i; ++j) {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
@@ -49,8 +44,7 @@ struct Route {
     int cityA, cityB, time;
 };
 
-int routeSort(const void* a, const void* b)
-{
+int routeSort(const void* a, const void* b) {
     struct Route* routeA = (struct Route*)a;
     struct Route* routeB = (struct Route*)b;
     int ret = 0;
@@ -65,25 +59,20 @@ int routeSort(const void* a, const void* b)
     return ret;
 }
 
-void createAndSortRoutes(struct Route* routes, int** matrix, int numCities, int numRoutes)
-{
+void createAndSortRoutes(struct Route* routes, int** matrix, int numCities, int numRoutes) {
     int routeNum = 0;
-    for (int i = 1; i <= (numCities - 1); ++i)
-    {
-        for (int j = 0; j < i; ++j)
-        {
+    for (int i = 1; i <= (numCities - 1); ++i) {
+        for (int j = 0; j < i; ++j) {
             struct Route* route = malloc(sizeof(struct Route));
-            if (route != NULL)
-            {
+            if (route != NULL) {
                 route->cityA = j;
                 route->cityB = i;
-                route->time = matrix[i-1][j];
+                route->time = matrix[i - 1][j];
                 printf("Route: %d -> %d   = %d\n", route->cityA, route->cityB, route->time);
                 routes[routeNum] = *route;
                 ++routeNum;
             }
-            else
-            {
+            else {
                 fprintf(stderr, "Failed to create Route on heap");
                 exit(-1);
             }
@@ -93,38 +82,37 @@ void createAndSortRoutes(struct Route* routes, int** matrix, int numCities, int 
     qsort(routes, numRoutes, sizeof(struct Route), routeSort);
 }
 
-int findMinimumTime(int** matrix, int numCities)
-{
-    int numberOfRoutes = (numCities * (numCities - 1)) / 2; // EQ for lower half of matrix
-    struct Route* routes = (struct Route*)malloc(numberOfRoutes * sizeof(struct Route));
+void DepthSearch(int n, bool* visited, int numCities, int** adj) {
+    visited[n] = true;
+    printf("visited: %d\n", n);
+    for (int i = 0; i < numCities; ++i) {
+        if (!visited[i]) {
+            DepthSearch(i, visited, numCities, adj);
+        }
+    }
+}
 
-    createAndSortRoutes(routes, matrix, numCities, numberOfRoutes);
-
-
-
-
-    printf("\n\nSorted \n");
-    for (int i = 0; i < numberOfRoutes; ++i)
-    {
-        printf("Route: %d -> %d   = %d\n", routes[i].cityA, routes[i].cityB, routes[i].time);
+int findMinimumTime(int** matrix, int numCities) {
+    bool* visited = calloc(numCities, sizeof(bool));
+    for (int i = 0; i < numCities; ++i) {
+        visited[i] = false;
     }
 
-    free(routes);
+    DepthSearch(numCities, visited, numCities, matrix);
     return 0;
 }
 
 /*
-Began this problem by reacquainting myself with C99 as it has been a long time since I have used it. 
+Began this problem by reacquainting myself with C99 as it has been a long time since I have used it.
 It took a little while to figure out the best dev environment but I went with visual studio even though its implementation of C99 is missing some features.
 
-I then proceeded to tackle some basics, reading standard input, parsing the input and storing it into a usable matrix format. 
+I then proceeded to tackle some basics, reading standard input, parsing the input and storing it into a usable matrix format.
 I then spent some time converting that matrix into a more useable format by using C99’s struct features.
 
-I then attempted to use Kruskals’s minimum spanning tree algorithm to attempt to 
+I then attempted to use Kruskals’s minimum spanning tree algorithm to attempt to
 find the shortest path from the “capital” to each city but it quickly became apparent that this was not the correct solution.
 */
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     char line[LINE_SIZE];
     int N = 0;
 
@@ -135,14 +123,12 @@ int main(int argc, char** argv)
 
     // Create an array of arrays size N
     int** matrix = (int**)malloc((N - 1) * sizeof(int));
-    for (int i = 0; i < (N - 1); i++)
-    {
+    for (int i = 0; i < (N - 1); i++) {
         matrix[i] = (int*)malloc((i + 1) * sizeof(int));
     }
 
     int i = 0;
-    while (fgets(line, LINE_SIZE, stdin))
-    {
+    while (fgets(line, LINE_SIZE, stdin)) {
         matrix[i] = readLine(line, matrix[i], i);
         ++i;
     }
